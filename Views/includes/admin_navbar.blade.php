4ad4d8b90210cdd2208a973591459f3602668d42
Felice <felice.ostuni@uania.com>
2023-03-23 15:12:01 +0100

    wip

--- Views/includes/admin_navbar.blade.php
+++ Views/includes/admin_navbar.blade.php
@@ -38,7 +38,7 @@
 
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="mr-2 d-none d-lg-inline text-gray-600 small">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->full_name }}</div>
                             @if(Auth::user()->company) <div class="text-center mt-n2"><small><em>{{Auth::user()->company->business_name}}</em></small></div> @endif
                     </div>
                     <img class="img-profile rounded-circle" src="{{ asset('vendor/layout/img/user-account-icon.png') }}">
@@ -53,6 +53,12 @@
                     </a>
                     @endif
 
+                    @impersonating
+                    <a class="dropdown-item" href="{{ route('impersonate.leave') }}">
+                        <i class="fas fa-user-slash fa-sm fa-fw mr-2 text-gray-400"></i>
+                        {{ __('Leave impersonation') }}
+                    </a>
+                    @endImpersonating
 
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
