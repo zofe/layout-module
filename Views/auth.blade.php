91d7c15523cc10b3597e58521c2eb0a6a27c05fc
Felice <felice.ostuni@uania.com>
2023-04-17 14:17:38 +0200

    wip

--- Views/auth.blade.php
+++ Views/auth.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+{{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
 {{--    <script src="{{ asset('vendor/layout/layout.js') }}" defer></script>--}}
 
     <!-- Fonts -->
